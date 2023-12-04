20e1304be9198ac9a4da96390049c8425876f1a0
Alex E.T. Snyder <alexetsnyder@gmail.com>
2023-12-04 11:20:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TerrariumKit
+ C++ and OpenGL project about simulating a small 2D world
---
+++ TerrariumKit/Main.cpp
@@ -0,0 +1,10 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+
+	return EXIT_SUCCESS;
+}
